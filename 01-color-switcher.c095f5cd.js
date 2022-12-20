3a4310e65360da65a3a761b96880d6c07b576888
YaroslavaSenko <110532091+YaroslavaSenko@users.noreply.github.com>
2022-12-20 21:22:58 +0000

    Deploying to gh-pages from @ YaroslavaSenko/goit-js-hw-09@db73db24df99d1d68cd450bc0cbb0f82362533dd 🚀

---
+++ 01-color-switcher.c095f5cd.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let o=null;t.addEventListener("click",(function(){o=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16)}`;r.style.backgroundColor=t}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.c095f5cd.js.map
