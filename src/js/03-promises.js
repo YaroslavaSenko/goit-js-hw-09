5661358b6e5f36fcfb7fbc6f92216b016d6fe40b
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2022-12-24 16:47:12 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector(".form");
+form.addEventListener("submit", onFormData);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+      return new Promise((resolve, reject) => {
+      setTimeout(() => { 
+        if (shouldResolve) {
+          resolve({position, delay});
+        } else {
+          reject({position, delay});
+        }
+      }, delay)
+  })
+  }
+
+function onFormData(evt) {
+  evt.preventDefault()
+
+  let {
+    elements: { delay, step, amount },
+  } = evt.currentTarget;
+  let enteredDelay = Number(delay.value);
+  let enteredStep = Number(step.value);
+  let enteredAmount = Number(amount.value);
+
+  for (let position = 1; position <= enteredAmount; position += 1) {
+    createPromise(position, enteredDelay)
+
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      })
+    enteredDelay += enteredStep;
   }
 }
